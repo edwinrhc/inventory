cd7a330c8c7ff0ac22c91129915b4229ff6df153
Edwin HC <edwinrhc@gmail.com>
2024-10-10 11:21:26 -0500

    Guardar Categorias

--- front-inventory/src/app/modules/category/components/category/category.component.ts
+++ front-inventory/src/app/modules/category/components/category/category.component.ts
@@ -3,6 +3,7 @@ import {CategoryService} from "../../../shared/services/category.service";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatDialog} from "@angular/material/dialog";
 import {NewCategoryComponent} from "../new-category/new-category.component";
+import {MatSnackBar, MatSnackBarRef, SimpleSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-category',
@@ -13,30 +14,31 @@ export class CategoryComponent implements OnInit {
 
   //Aquí se inyecta el servicio en
   private categoryService = inject(CategoryService);
-  public dialog  = inject(MatDialog);
+  public dialog = inject(MatDialog);
+  private snackBar = inject(MatSnackBar);
 
   ngOnInit(): void {
-       this.getCategories();
-    }
-
-  displayedColumns: string[] = ['id','name','description','actions'];
-  dataSource = new MatTableDataSource<CategoryElement>();
-
-  getCategories():void{
-    this.categoryService.getCategories()
-      .subscribe((data:any)=>{
-      console.log("Respuesta categories: ", data);
-      this.processCategoriesResponse(data);
-    },(error:any) => {
-      console.log("error: ", error);
-    })
+    this.getCategories();
   }
 
-  processCategoriesResponse(resp: any){
+  displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
+  dataSource = new MatTableDataSource<CategoryElement>();
+
+  getCategories(): void {
+    this.categoryService.getCategories()
+      .subscribe((data: any) => {
+        console.log("Respuesta categories: ", data);
+        this.processCategoriesResponse(data);
+      }, (error: any) => {
+        console.log("error: ", error);
+      })
+  }
+
+  processCategoriesResponse(resp: any) {
     const dataCategory: CategoryElement[] = [];
-    if(resp.metadata[0].code == "00"){
+    if (resp.metadata[0].code == "00") {
       let listCategory = resp.categoryResponse.category;
-      listCategory.forEach((element:CategoryElement) => {
+      listCategory.forEach((element: CategoryElement) => {
         dataCategory.push(element)
       });
       this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
@@ -44,18 +46,30 @@ export class CategoryComponent implements OnInit {
   }
 
 
-  openCategoryDialog(){
+  openCategoryDialog() {
     const dialogRef = this.dialog.open(NewCategoryComponent, {
       width: '450px',
 
     });
 
-    dialogRef.afterClosed().subscribe((result:any) => {
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result == 1) {
+        this.openSnackBar("Categoria Agregada","Exitosa");
+        this.getCategories();
+      } else if (result == 2) {
+        this.openSnackBar("Se produjo un error al guardar Categoria","Error");
+      }
+
+    });
+  }
+
+  openSnackBar(message: string, action: string): MatSnackBarRef<SimpleSnackBar> {
+    return this.snackBar.open(message, action,{
+      duration: 2000
     });
   }
 
 }
-
 
 
 export interface CategoryElement {
