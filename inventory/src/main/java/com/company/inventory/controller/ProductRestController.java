b8367bc301993d3dbecdd4c8d7516768795586cf
Edwin HC <edwinrhc@gmail.com>
2024-10-23 16:58:02 -0500

    Modulo Product . Picture Long , Basic Blog

--- inventory/src/main/java/com/company/inventory/controller/ProductRestController.java
+++ inventory/src/main/java/com/company/inventory/controller/ProductRestController.java
@@ -1,7 +1,51 @@
 package com.company.inventory.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.company.inventory.model.Product;
+import com.company.inventory.response.ProductResponseRest;
+import com.company.inventory.service.IProductService;
+import com.company.inventory.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
+@RequestMapping("/api/v1")
 public class ProductRestController {
+
+    @Autowired
+    IProductService service;
+
+    /**
+     * @param picture
+     * @param name
+     * @param price
+     * @param account
+     * @param categoryId
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/products")
+    public ResponseEntity<ProductResponseRest> saveProduct(
+            @RequestParam("picture") MultipartFile picture,
+            @RequestParam("name") String name,
+            @RequestParam("price") int price,
+            @RequestParam("account") int account,
+            @RequestParam("categoryId") Long categoryId) throws IOException {
+
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setAccount(account);
+        product.setPicture(Util.compressZLib(picture.getBytes()));
+
+        ResponseEntity<ProductResponseRest> response = service.save(product, categoryId);
+
+        return response;
+    }
+
 }
--- inventory/src/main/java/com/company/inventory/response/ProductResponse.java
+++ inventory/src/main/java/com/company/inventory/response/ProductResponse.java
@@ -1,4 +1,17 @@
 package com.company.inventory.response;
 
+import com.company.inventory.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
+
+    List<Product> products;
+
 }
